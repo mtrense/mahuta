fc46ba6a61f6353d5bd33c779934d91adcb6a8f2
Max Trense <dev@trense.info>
2017-06-18 17:12:53 +0200

    Adds specs for Mahuta::Node

--- spec/mahuta/node_spec.rb
+++ spec/mahuta/node_spec.rb
@@ -15,6 +15,43 @@
 
 RSpec.describe Mahuta::Node do
   
+  let :schema do
+    Mahuta::Schema.new do
+      type :root do
+        def test_node!(&block)
+          add_child(:test_node, &block)
+        end
+      end
+      type :test_node do
+        def test_node!(&block)
+          add_child(:test_node, &block)
+        end
+      end
+    end
+  end
   
+  context 'A single node' do
+    subject do
+      Mahuta::Node.new nil, schema, :root
+    end
+    
+    it('should have no parent') { expect(subject.parent).to be_nil }
+    it('should have no children') { expect(subject.children).to be_empty }
+    it('should be leaf') { expect(subject.leaf?).to be_truthy }
+    it('should be root') { expect(subject.root?).to be_truthy }
+    
+  end
+  
+  context 'A root node with one child node' do
+    subject do
+      puts " >> #{schema.inspect} <<"
+      schema.new do
+        test_node!
+      end
+    end
+    
+    it { expect(subject.children).not_to be_empty }
+    
+  end
   
 end
