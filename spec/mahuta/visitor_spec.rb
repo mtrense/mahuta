7adb01de88f76d43358ceeb1e6d0595a7edb2be8
Max Trense <dev@trense.info>
2017-06-19 08:09:01 +0200

    Adds test for Mahuta::Visitor

--- spec/mahuta/visitor_spec.rb
+++ spec/mahuta/visitor_spec.rb
@@ -15,6 +15,34 @@
 
 RSpec.describe Mahuta::Visitor do
   
+  #
+  # No spies here, they always create methods with arity -1, which is tested 
+  # against in Visitor. But using varargs is not the common use case here...
+  # 
+  subject do
+    Class.new.tap do |cl|
+      cl.class_exec do
+        include Mahuta::Visitor
+        def enter_root(node, depth)
+          @captured_enter = [node, depth]
+        end
+        def leave_root(node, depth)
+          @captured_leave = [node]
+        end
+        attr_reader :captured_enter, :captured_leave
+      end
+    end.new
+  end
   
+  it do
+    node = Object.new
+    def node.node_type
+      :root
+    end
+    subject.enter(node, 0)
+    subject.leave(node, 0)
+    expect(subject.captured_enter).to contain_exactly(node, 0)
+    expect(subject.captured_leave).to contain_exactly(node)
+  end
   
 end
