b03d008a3ee86c440aa774ee4833d09e556baef6
Max Trense <dev@trense.info>
2017-07-21 15:22:19 +0200

    Adds support for coloring type names

--- lib/mahuta/utils/tree_printer.rb
+++ lib/mahuta/utils/tree_printer.rb
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 require 'pastel'
+require 'byebug'
 
 module Mahuta::Utils
   
@@ -28,18 +29,30 @@ module Mahuta::Utils
     end
     
     EXTENDED_COLORIZED_FORMAT = proc do |node, depth|
-      attrs = node.attributes.collect {|n, v| "#{P.yellow(n)} = #{P.cyan(v.inspect)}" }
-      '  '*depth + P.bold(node.leaf? ? '-' : '+') + " [#{P.bold.blue(node.node_type.to_s)}] " + attrs.join(' ')
+      attrs = node.attributes.collect {|n, v| "#{P.white(n)}=#{P.cyan(v.inspect)}" }
+      '  '*depth + P.bold(node.leaf? ? '-' : '+') + " [#{__type_color(node)}] " + attrs.join(' ')
     end
     
     def initialize(options = {}, &filter_block)
       @out = options[:out] || $stdout
       @format = options[:format] || EXTENDED_COLORIZED_FORMAT
       @filter = filter_block || Proc.new { true }
+      @type_colors = options.delete(:type_colors) || proc { [:bold, :blue] }
+      @key_colors = options.delete(:key_colors) || proc { [:yellow] }
+      @value_colors = options.delete(:value_colors) || proc { [:cyan] }
     end
     
     def enter(node, depth)
-      @out.puts @format.call(node, depth) if @filter.call(node)
+      @out.puts instance_exec(node, depth, &@format) if @filter.call(node)
+    end
+    
+    private def __type_color(node)
+      case @type_colors
+      when Hash
+        P.decorate(node.node_type.to_s, *@type_colors[node.node_type])
+      when Proc
+        P.decorate(node.node_type.to_s, instance_exec(node.node_type, node, &@type_colors))
+      end
     end
     
   end
