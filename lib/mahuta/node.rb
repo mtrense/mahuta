7dbd7c69725d2c9be658ebe06bc51a174a914b13
Max Trense <dev@trense.info>
2017-07-25 09:27:02 +0200

    Fixes cases where node has no name

--- lib/mahuta/node.rb
+++ lib/mahuta/node.rb
@@ -153,7 +153,7 @@ module Mahuta
     end
     
     def inspect
-      "#<Mahuta::Node:0x#{object_id.to_s(16)} type:#{node_type} name:#{name}>"
+      "#<Mahuta::Node:0x#{object_id.to_s(16)} type:#{node_type}#{self[:name] ? " name:#{self[:name]}" : ''}>"
     end
     
     private def __defining_location
